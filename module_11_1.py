ebda9eb64fce1950274859a9fe7de4983619d70f
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-07-31 19:44:48 +0500

    Koch snowflake 2

--- module_11_1.py
+++ module_11_1.py
@@ -3,16 +3,7 @@ import numpy as np
 
 
 def koch_snowflake(order, scale=10):
-    """
-    Return two lists x, y of point coordinates of the Koch snowflake.
 
-    Parameters
-    ----------
-    order : int
-        The recursion depth.
-    scale : float
-        The extent of the snowflake (edge length of the base triangle).
-    """
     def _koch_snowflake_complex(order):
         if order == 0:
             # initial triangle
@@ -21,9 +12,9 @@ def koch_snowflake(order, scale=10):
         else:
             ZR = 0.5 - 0.5j * np.sqrt(3) / 3
 
-            p1 = _koch_snowflake_complex(order - 1)  # start points
-            p2 = np.roll(p1, shift=-1)  # end points
-            dp = p2 - p1  # connection vectors
+            p1 = _koch_snowflake_complex(order - 1)
+            p2 = np.roll(p1, shift=-1)
+            dp = p2 - p1
 
             new_points = np.empty(len(p1) * 4, dtype=np.complex128)
             new_points[::4] = p1
@@ -36,18 +27,9 @@ def koch_snowflake(order, scale=10):
     x, y = points.real, points.imag
     return x, y
 
-
-"""x, y = koch_snowflake(order=5)
-
-plt.figure(figsize=(8, 8))
-plt.axis('equal')
-plt.fill(x, y)
-plt.show()"""
-
 x, y = koch_snowflake(order=2)
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(9, 3),
-                                    subplot_kw={'aspect': 'equal'})
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
 ax1.fill(x, y)
 ax2.fill(x, y, facecolor='lightsalmon', edgecolor='orangered', linewidth=3)
 ax3.fill(x, y, facecolor='none', edgecolor='purple', linewidth=3)
